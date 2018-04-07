2b4fd7213b0d8c1cc6092d1e44e8659a3feed8bc
Carl Pearson <carl.w.pearson@gmail.com>
2018-04-07 16:43:22 -0500

    use plot.yml to configure plot

--- figures/make_plot.py
+++ figures/make_plot.py
@@ -3,17 +3,49 @@
 import sys
 import pandas as pd
 import matplotlib.pylab as plt
+import yaml
+import os.path as path
+
+def key_exists(element, *keys):
+    if type(element) is not dict:
+        raise AttributeError('keys_exists() expects dict as first argument.')
+    if len(keys) == 0:
+        raise AttributeError('keys_exists() expects at least two arguments, one given.')
+
+    _element = element
+    for key in keys:
+        try:
+            _element = _element[key]
+        except KeyError:
+            return False
+    return True
 
 if len(sys.argv) < 3:
     sys.exit(1)
 
 csv_path = sys.argv[1]
+out_path = sys.argv[2]
+
+
+
+
+## Look for a yaml file in the same dir as the csv file
+yaml_file = path.join(path.dirname(csv_path), "plot.yml")
+
+if path.exists(yaml_file):
+    with open(yaml_file, 'rb') as f:
+        cfg = yaml.load(f)
+        print cfg
 
 data = pd.read_csv(csv_path, index_col=0)
 
 ax = data.plot(kind='line')
 
-ax.set_xscale("log", nonposx='clip')
+if key_exists(cfg, "xaxis", "scale"):
+    ax.set_xscale(cfg["xaxis"]["scale"], nonposx='clip')
+
+if key_exists(cfg, "yaxis", "label"):
+    ax.set_ylabel(cfg["yaxis"]["label"])
 
 # plt.show()
 
