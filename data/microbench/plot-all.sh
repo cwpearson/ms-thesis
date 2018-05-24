a3a2bde54790f44b0a7c18fe510b9a509b2f4fed
Carl Pearson <pearson@illinois.edu>
2018-05-24 17:16:27 -0500

    hal gpu-gpu data

--- data/microbench/plot-all.sh
+++ data/microbench/plot-all.sh
@@ -10,3 +10,11 @@ for y in *.yml; do
     fi
 done
 wait
+
+for y in *.yml; do
+    pdf="${y%%.*}.pdf";
+    yml=$y;
+    if [ $yml -nt $pdf ]; then
+        nice -n20 ./plot.py "${y%%.*}.pdf" "$y"
+    fi
+done
